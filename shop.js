5352c8ce69a852d6da27851bbe746ea7dc524739
annshkatova <annash2007@gmail.com>
2024-12-04 03:51:36 +0300

    Update shop.js

--- shop.js
+++ shop.js
@@ -53,7 +53,7 @@ async function getResponse() {
                 <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
                 <img style="width: 180px" class="align-self-center" src=${content_filter[key].img}>
                 <h5 class="card-title">${content_filter[key].title}</h5>
-                <p class="card-text">${content_filter[key].description}. Цена ${content_filter[key].price} р.</p>
+                <p class="card-text">${content_filter[key].description} <br> Цена ${content_filter[key].price}</p>
                 <input type="hidden" name= "vendor_code" value=${content_filter[key].vendor_code}>
                 <p class="card-text" >Заказать<input class="w-25" type="text" value="0" name="check"></p>
                 </li>
@@ -105,7 +105,7 @@ async function getResponse1() {
                 <li style="width: 310px" class="d-flex flex-column m-1 p-1 border bg-body">
                 <img style="width: 180px" class="align-self-center" src=${content_filter[key].img}>
                 <h5 class="card-title">${content_filter[key].title}</h5>
-                <p class="card-text">${content_filter[key].description}. Цена ${content_filter[key].price} р.</p>
+                <p class="card-text">${content_filter[key].description} <br> Цена ${content_filter[key].price}</p>
                 <input type="hidden" name= "vendor_code" value=${content_filter[key].vendor_code}>
                 <p class="card-text" >Заказать <input class="w-25" type="checkbox" name="check" value="0" onClick='this.value = this.checked ? 1 : 0'></p>
                 </li>
